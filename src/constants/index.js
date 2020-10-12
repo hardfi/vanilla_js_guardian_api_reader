088c2471d619527b156f2c97ae89a22fbeec46ce
Kamil Polok <k.polok@inwebit.pl>
2020-10-12 11:08:29 +0200

    T2: fixing api key

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,4 @@
-export const GUARDIAN_API_KEY = '90d8f2f2-653b-4c41-85af-4589dd572e0';
+export const GUARDIAN_API_KEY = '90d8f2f2-653b-4c41-85af-4589dd572e09';
 export const BASE_URL = 'https://content.guardianapis.com/search';
 export const DAYS_BACK = 30;
 export const PAGE_SIZE = 10;
